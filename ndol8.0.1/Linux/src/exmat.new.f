d04ad635c53f1bc17bf14c2a5714ff06ac331c7f
imtvi06 <imtvi06@gmail.com>
2023-12-20 10:37:59 +0100

    Finalmente resuelto el problema de LCI y FCIS con exmat.f. Era un probblema de las dimensiones de la variable PO. Renovado 8.0.1 en todos los sitios

--- ndol8.0.1/Linux/src/exmat.new.f
+++ ndol8.0.1/Linux/src/exmat.new.f
@@ -32,13 +32,13 @@
       CHARACTER*5 state5
       CHARACTER*6 state6
       CHARACTER*3 qs/'_QS'/, qt/'_QT'/, stq
-      CHARACTER*150 qjmol/'jmolscript:isosurface resolution 5 molecular
-     & 0.0 map MEP translucent; background white; color isosurface range
-     & -0.1 0.1'/
-      CHARACTER*150 dqjmol/'jmolscript:isosurface resolution 5 molecular
-     &  0.0 map MEP translucent; background white; color isosurface rang
-     & e -0.02 0.02'/
-      DIMENSION C(N,N),PO(KORD,*),PEII(*),DEX(NFCI,*),
+      CHARACTER*150 qjmol/'jmolscript:isosurface resolution 10 molecular
+     & 0.0 map MEP; background white; color isosurface range -0.1 0.1;
+     & isosurface translucent'/,
+     & dqjmol/'jmolscript:isosurface resolution 10 molecular
+     & 0.0 map MEP; background white; color isosurface range -0.05 0.05;
+     & isosurface translucent'/
+      DIMENSION C(N,N),PO(KORD,KORD),PEII(*),DEX(NFCI,*),
      &          P(NA,2),PE(NA,2),XC(NA),YC(NA),ZC(NA),
      &          INDI(*),JNDI(*)
       PARAMETER (CERO=0.D0, DOS=2.D0, EVN=.8065817D0, EINV=1.D+03,      
@@ -48,37 +48,34 @@
 * Se salvan los vectores de coeficientes cuadráticos CI de los iopt(3)
 * primeros estados en DEX. "k" es el estado CI y "l" la configuracion
 * SCF
-      if (iabs(iopt(3)).gt.iopt(8)) then
+      if (abs(iopt(3)).gt.iopt(8)) then
         nconf = iopt(8)
       else
-        nconf = iabs(iopt(3))
+        nconf = abs(iopt(3))
       endif        
       do k=1,nconf
-*      do k=1,kord
         do l=1,kord
           akl = po(k,l)
           dex(k,l) = akl*akl
         enddo
       enddo
 
-
-* ESTE ES EL LAZO PRINCIPAL SOBRE CADA ESTADO "k" DE CI
+* ESTE ES EL LAZO PRINCIPAL SOBRE CADA ESTADO "k"
 
       do k=1,nconf
         write (iw,120) k,eee(k),fr(k),slam(k),fo(k),isymt(ksym(k))
 
 * Anulacion inicial total de la matriz PO
 
-        do i=1,N
-          PEII(i) = CERO
-          do j=1,N
+        do i=1,n
+          do j=1,n
             po(i,j) = CERO
           enddo
         enddo
 
-* Calculo del estado CI "k" iterando sobre cada estado SCF "l"
+* Calculo del estado CI "k" iterando sobre cada estado "l"
 
-        do l=1,KORD
+        do l=1,kord
           indil = indi(l)
           jndil = jndi(l)
 
@@ -86,14 +83,14 @@
 * Se guarda sobre la mitad mu.gt.nu de po. La diagonal se guarda en el
 * vector peii. 
 
-          do mu=1,N
-            do nu=1,mu
+          do 10 mu=1,n
+            do 10 nu=1,mu
               sum = CERO
-              do i=1,INDIL-1
+              do i=1,indil-1
                 sum = sum + DOS*c(mu,i)*c(nu,i)
               enddo
               sum = sum + c(mu,indil)*c(nu,indil)
-              do i=INDIL+1,NOCC
+              do i=indil+1,nocc
                 sum = sum + DOS*c(mu,i)*c(nu,i)
               enddo
               sum = sum + c(mu,jndil)*c(nu,jndil)
@@ -102,24 +99,17 @@
               else
                 peii(mu) = sum
               endif
-            enddo
-          enddo
+10        continue
 
 * Creación de la matriz de densidad CI en la mitad mu.le.nu de la matriz
 * En este lazo se crea e incrementa para cada configuracion "l" segun
 * su peso en "k" dado por dex.
  
-          do mu=2,N
-            do nu=1,mu-1
-              po(nu,mu) = po(nu,mu) + dex(k,l)*po(mu,nu)
-            enddo
-          enddo  
-          do mu=1,N
-            po(mu,mu) = po(mu,mu) + dex(k,l)*peii(mu)
-*      write (*,'(a,i3,a,i3,a,i3,a,3f10.5)')
-*     &' estado CI k,SCF l, orbital mu, dex(k,l), peii(mu), po(mu,mu)',
-*     &k,',',l,',',mu,',',dex(k,l), peii(mu),po(mu,mu)
-          enddo
+          do 20 mu=2,n
+            do 20 nu=1,mu-1
+20        po(nu,mu) = po(nu,mu) + dex(k,l)*po(mu,nu)
+          do 30 mu=1,n
+30           po(mu,mu) = po(mu,mu) + dex(k,l)*peii(mu)
         enddo
 
 * VECTORES DE DENSIDAD ATOMICA INTEGRAL EN ESTADOS EXCITADOS
@@ -130,7 +120,7 @@
           pe(i,1) = CERO
           pe(i,2) = CERO
         enddo
-        DO I=1,NA
+        DO 100 I=1,NA
           MU = NO1(I)
           LI = NAT(I)
           QII = LI.GT.2
@@ -141,20 +131,19 @@
             MU3 = MU + 3
             pe(i,2) = PO(MU1,MU1) + PO(MU2,MU2) + PO(MU3,MU3)
           endif
-        enddo
+100     CONTINUE
 
 * IMPRESION DE LAS DENSIDADES DE CARGA ATOMICA DEL ESTADO k
 
         WRITE (IW,1111) k
-        DO I=1,NA
+        DO 334 I=1,NA
           LI = NAT(I)
-          WRITE (IW,1112) I,iatom(LI),PE(I,1),PE(I,2),PE(I,1)+PE(I,2),
+334       WRITE (IW,1112) I,iatom(LI),PE(I,1),PE(I,2),PE(I,1)+PE(I,2),
      &                   ANS(LI)-PE(I,1), ANP(LI)-PE(I,2),
      &                   ANV(LI)-(PE(I,1)+PE(I,2)),
      &                   P(I,1)-PE(I,1),P(I,2)-PE(I,2),
      &                   (P(I,1)+P(I,2))-(PE(I,1)+PE(I,2))
-        ENDDO
-        
+
 * SALIDA DE FICHEROS PARA EL MAPA DE LAS DENSIDADES DE CARGA CON JMOL
 
        if (QQMAP) then
@@ -191,7 +180,7 @@
      &                            (P(jj,1)+P(jj,2))-(PE(jj,1)+PE(jj,2))
          enddo
          close (52)
-        endif     
+      endif     
 
 * IMPRESION DEL MOMENTO DIPOLO DEL ESTADO EXCITADO
 
@@ -206,6 +195,7 @@
           write (iw,121) k
           call pegleg (kord,po)
         endif
+
       enddo
 
 * AQUI TERMINA EL LAZO SOBRE CADA ESTADO "k"
